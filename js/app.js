e34373b4f7a4e5f749bf2ce512be5e3d8958ac9b
David <david.diez31@free.fr>
2017-02-09 23:56:56 +0100

    first commit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,42 @@
-(function(){
-	"use strict";
-	var app = {
-		init:function(){
-			// let's go
-		},
-	};
+(function() {
+    "use strict";
+    var app = {
+        init: function() {
+            // let's go
+        },
+    };
+
+    $(document).ready(function() {
+        app.init();
+
+        $.ajax({
+            type: "GET",
+            url: "example.md",
+            success: convert,
+            error: function() {
+                alert("404 Not Found - Oops something went wrong !");
+            }
+        });
+
+        function convert(data) {
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(data);
+            $("#md").append(html);
+        };
 
 
-	$(document).ready(function(){
-		app.init();
-	});
+        $.getJSON("menu.json", function(data) {
+            var blog = data.blog;
+            var menu = data.menu;
+            $("#app").append("<div><h1>" + blog + "</h1></div>");
+            for (var i = 0; i < data.menu.length; i++) {
+                $("#app").append("<div class='posts'><a href='" + menu[i].href + "'target='_blank'>" + menu[i].title + "</a></div>");
+            }
+        });
+
+
+
+
+
+    });
 })();
